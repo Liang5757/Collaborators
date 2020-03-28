f44db0138579d723c9f4f9861b01a86be6a72bbf
P4XL <zeongpaang@outlook.com>
2020-03-29 07:52:57 +0800

    debug

--- utils/paser.py
+++ utils/paser.py
@@ -1,9 +1,18 @@
 # 指令解析
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument('-n', '-N', nargs=1, type=int, action='store_true', help='storage number n')
 
-args = parser.parse_args()
+def arg_parse():
+    parser = ArgumentParser()
+    parser.add_argument('-n', nargs=1, type=int, help='the num of generated subject')
+    parser.add_argument('-r', nargs=1, type=int, help='the range of generated numbers. Range: [1, R]')
+    parser.add_argument('-e', nargs=1, type=str, help='exercise file')
+    parser.add_argument('-a', nargs=1, type=str, help='answer file')
+    parser.add_argument('-g', help='GUI mode')
+    args = parser.parse_args()
+    # print(args)
+    return args
 
-print(args)
+
+if __name__ == '__main__':
+    arg_parse()
